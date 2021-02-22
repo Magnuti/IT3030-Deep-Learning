100f9d1026e157a5e6eb1b783d9a37b08aa56e24
Magnuti <magnustidemann@gmail.com>
2021-02-22 13:49:52 +0100

    Sequence data generator added

--- project_2/data_generator.py
+++ project_2/data_generator.py
@@ -1,5 +1,4 @@
 from enum import Enum
-from random import randint
 import numpy as np
 import matplotlib.pyplot as plt
 import pathlib
@@ -10,20 +9,20 @@ from data_generator_parser import DataGeneratorArguments
 
 
 class Classes(Enum):
-    CIRCLE = 0
-    RECTANGLE = 1
-    VERTICAL_BARS = 2
-    CROSS = 3
+    ONE_RIGHT = 0
+    TWO_RIGHT = 1
+    ONE_LEFT = 2
+    TWO_LEFT = 3
 
 
 class DataGenerator:
     def __init__(self, args):
         self.args = args
 
-        self.output_dir = pathlib.Path("images")
+        self.output_dir = pathlib.Path("sequences")
         self.output_dir.mkdir(exist_ok=True)
 
-    def get_images(self, flatten=True):
+    def get_sequences(self):
         """
         Returns data and targets.
         """
@@ -43,9 +42,6 @@ class DataGenerator:
             image = plt.imread(self.output_dir.joinpath(f))
             # Apparently this returns a (M, N, 4) array because it thinks it is a  RGBA image..
             image = image[:, :, 0]
-
-            if flatten:
-                image = image.flatten()
 
             data.append(image)
             targets.append(target)
@@ -74,24 +70,17 @@ class DataGenerator:
 
         plt.show()
 
-    def create_figures(self):
-
-        circles = self.__create_circles(self.args.circle_radius_range)
-
-        rectangles = self.__create_rectangles(
-            self.args.rectanlge_range_height, self.args.rectanlge_range_width)
-
-        vertical_bars = self.__create_vertical_bars(
-            self.args.vertical_bar_width)
-
-        crosses = self.__create_crosses(
-            self.args.cross_size_range, self.args.cross_thickness_range)
+    def generate_sequences(self):
+        one_rights = self.__create_sequences(1)
+        two_rights = self.__create_sequences(2)
+        one_lefts = self.__create_sequences(-1)
+        two_lefts = self.__create_sequences(-2)
 
         # Add noise
-        circles = self.__add_noise(circles)
-        rectangles = self.__add_noise(rectangles)
-        vertical_bars = self.__add_noise(vertical_bars)
-        crosses = self.__add_noise(crosses)
+        one_rights = self.__add_noise(one_rights)
+        two_rights = self.__add_noise(two_rights)
+        one_lefts = self.__add_noise(one_lefts)
+        two_lefts = self.__add_noise(two_lefts)
 
         def save_figures(figures, figure_name):
             for i, fig in enumerate(figures):
@@ -99,174 +88,54 @@ class DataGenerator:
                 plt.imsave(self.output_dir.joinpath(
                     "{}_{}.png".format(figure_name, i)), fig, cmap="gray")
 
-        # Save images
-        save_figures(circles, Classes.CIRCLE.name.lower())
-        save_figures(rectangles, Classes.RECTANGLE.name.lower())
-        save_figures(vertical_bars, Classes.VERTICAL_BARS.name.lower())
-        save_figures(crosses, Classes.CROSS.name.lower())
+        # Save sequences
+        save_figures(one_rights, Classes.ONE_RIGHT.name.lower())
+        save_figures(two_rights, Classes.TWO_RIGHT.name.lower())
+        save_figures(one_lefts, Classes.ONE_LEFT.name.lower())
+        save_figures(two_lefts, Classes.TWO_LEFT.name.lower())
 
-    def __add_noise(self, images):
-        for i, image in enumerate(images):
-            for k in range(round(self.args.image_dimension * self.args.image_dimension * self.args.noise_ratio)):
-                x = randint(0, self.args.image_dimension - 1)
-                y = randint(0, self.args.image_dimension - 1)
-                images[i][y, x] = not image[y, x]
+    def __add_noise(self, sequences):
+        for i, sequence in enumerate(sequences):
+            for k in range(round(self.args.pattern_length * self.args.sequence_length * self.args.noise_ratio)):
+                x = np.random.randint(0, self.args.pattern_length)
+                y = np.random.randint(0, self.args.sequence_length)
+                sequences[i][y, x] = not sequence[y, x]
 
-        return images
+        return sequences
 
-    def __create_circles(self, circle_radius_range):
-        circles = []
-        for i in range(self.args.images_in_each_class):
-            if(self.args.center):
-                center_x = (self.args.image_dimension // 2)
-                center_y = center_x
-                if(self.args.image_dimension % 2 == 0):
-                    max_radius = self.args.image_dimension / 2 - 1
-                else:
-                    max_radius = self.args.image_dimension // 2
-                min_radius = circle_radius_range[0]
-                max_radius = min(max_radius, circle_radius_range[1])
-                radius = randint(min_radius, max_radius)
-            else:
-                center_x = randint(
-                    circle_radius_range[0], self.args.image_dimension - circle_radius_range[0] - 1)
-                center_y = randint(
-                    circle_radius_range[0], self.args.image_dimension - circle_radius_range[0] - 1)
-                max_radius = min(self.args.image_dimension - center_x - 1,
-                                 center_x, self.args.image_dimension - center_y - 1, center_y)
-                max_radius = min(max_radius, circle_radius_range[1])
-                radius = randint(circle_radius_range[0], max_radius)
+    def __create_sequences(self, shift, threshold=0.7):
+        sequences = []
+        for _ in range(self.args.sequences):
+            sequence = np.empty(
+                (self.args.sequence_length, self.args.pattern_length))
 
-            img = np.zeros((self.args.image_dimension,
-                            self.args.image_dimension))
+            start_sequence = np.random.rand(self.args.pattern_length)
 
-            for degree in range(0, 360, 5):
-                x = round(radius * math.cos(math.radians(degree)))
-                y = round(radius * math.sin(math.radians(degree)))
-                img[center_y + y, center_x + x] = 1
+            # Adjust the number of 0s in the sequence.
+            # If threshold == 0.5 we get an even distribution of 1s and 0s
+            start_sequence[start_sequence <= threshold] = 0
+            start_sequence[start_sequence > threshold] = 1
 
-            circles.append(img)
+            # We do not want entirely black or white sequences
+            while np.count_nonzero(start_sequence) == 0 or np.count_nonzero(start_sequence) == self.args.pattern_length:
+                start_sequence = np.random.rand(self.args.pattern_length)
 
-        return circles
+                start_sequence[start_sequence <= threshold] = 0
+                start_sequence[start_sequence > threshold] = 1
 
-    def __create_rectangles(self, rectanlge_range_height, rectanlge_range_width):
-        rectangles = []
-        for i in range(self.args.images_in_each_class):
-            if(self.args.center):
-                center_x = (self.args.image_dimension // 2)
-                center_y = center_x
-                if(self.args.image_dimension % 2 == 0):
-                    max_half_height = self.args.image_dimension / 2 - 1
-                    max_half_width = max_half_height
-                else:
-                    max_half_height = self.args.image_dimension // 2
-                    max_half_width = max_half_height
+            sequence[0] = start_sequence
 
-                max_half_height = min(
-                    max_half_height, rectanlge_range_height[1] // 2)
-                max_half_width = min(
-                    max_half_width, rectanlge_range_width[1] // 2)
+            for i in range(1, self.args.sequence_length):
+                sequence[i] = np.roll(sequence[i - 1], shift)
 
-                height = randint(
-                    rectanlge_range_height[0] // 2, max_half_height)
-                width = randint(rectanlge_range_width[0] // 2, max_half_width)
-
-                start_y = center_y - height
-                end_y = center_y + height
-                start_x = center_x - width
-                end_x = center_x + width
-            else:
-                start_y = randint(0, self.args.image_dimension -
-                                  rectanlge_range_height[0] - 1)
-                start_x = randint(0, self.args.image_dimension -
-                                  rectanlge_range_width[0] - 1)
-
-                max_y = min(self.args.image_dimension - 1,
-                            start_y + rectanlge_range_height[1])
-                max_x = min(self.args.image_dimension - 1,
-                            start_x + rectanlge_range_width[1])
-
-                end_y = randint(start_y + rectanlge_range_height[0], max_y)
-                end_x = randint(start_x + rectanlge_range_width[0], max_x)
-
-            img = np.zeros((self.args.image_dimension,
-                            self.args.image_dimension))
-
-            img[start_y:end_y, start_x] = 1
-            img[start_y:end_y, end_x] = 1
-            img[start_y, start_x:end_x] = 1
-            img[end_y, start_x:end_x] = 1
-
-            img[end_y, end_x] = 1  # Hotfix for the corner pixel bottom-right
-
-            rectangles.append(img)
-
-        return rectangles
-
-    def __create_vertical_bars(self, vertical_bar_width):
-        vertical_bars = []
-        for i in range(self.args.images_in_each_class):
-            this_bar_width = randint(
-                vertical_bar_width[0], vertical_bar_width[1])
-
-            indexes = []
-            k = 0
-            for j in range(self.args.image_dimension):
-                if k < this_bar_width:
-                    indexes.append(j)
-
-                k += 1
-                if k >= this_bar_width * 4:
-                    k = 0
-
-            img = np.zeros((self.args.image_dimension,
-                            self.args.image_dimension))
-
-            img[indexes] = 1
-            img = np.roll(img, randint(0, self.args.image_dimension), axis=0)
-
-            vertical_bars.append(img)
-
-        return vertical_bars
-
-    def __create_crosses(self, cross_size_range, cross_thickness_range):
-        crosses = []
-        for i in range(self.args.images_in_each_class):
-            size = randint(cross_size_range[0], min(
-                cross_size_range[1], self.args.image_dimension))
-
-            if(self.args.center):
-                center_y = self.args.image_dimension // 2
-                center_x = center_y
-            else:
-                center_y = randint(
-                    size // 2, self.args.image_dimension - size // 2)
-                center_x = randint(
-                    size // 2, self.args.image_dimension - size // 2)
-
-            img = np.zeros((self.args.image_dimension,
-                            self.args.image_dimension))
-
-            thickness = randint(
-                cross_thickness_range[0], cross_thickness_range[1])
-            size_1 = math.floor(size / 2)
-            size_2 = math.ceil(size / 2)
-            thickness_1 = math.floor(thickness / 2)
-            thickness_2 = math.ceil(thickness / 2)
-            img[center_y - size_1:center_y + size_2, center_x -
-                thickness_1: center_x + thickness_2] = 1
-            img[center_y - thickness_1:center_y + thickness_2,
-                center_x - size_1: center_x + size_2] = 1
-
-            crosses.append(img)
-
-        return crosses
+            sequences.append(sequence)
+        return sequences
 
 
 if __name__ == "__main__":
     args = DataGeneratorArguments()
     args.parse_arguments()
     data_generator = DataGenerator(args)
-    data_generator.create_figures()
-    # data, targets = data_generator.get_images()
+    data_generator.generate_sequences()
+    # data, targets = data_generator.get_sequences()
     data_generator.random_sample()
--- project_2/data_generator_parser.py
+++ project_2/data_generator_parser.py
@@ -6,26 +6,10 @@ class DataGeneratorArguments:
         with open("data_generator_config.yaml", "r", encoding="utf-8") as f:
             config_data = yaml.safe_load(f)
 
-        self.training_ratio = config_data["training_ratio"]
-        self.validation_ratio = config_data["validation_ratio"]
-        self.testing_ratio = config_data["testing_ratio"]
-        self.min_image_dimension = config_data["min_image_dimension"]
-        self.max_image_dimension = config_data["max_image_dimension"]
-        self.image_dimension = config_data["image_dimension"]
-
-        if(self.image_dimension < self.min_image_dimension or self.image_dimension > self.max_image_dimension):
-            raise ValueError("Invalid image dimension")
-
-        self.circle_radius_range = config_data["circle_radius_range"]
-        self.rectanlge_range_height = config_data["rectanlge_range_height"]
-        self.rectanlge_range_width = config_data["rectanlge_range_width"]
-        self.vertical_bar_width = config_data["vertical_bar_width"]
-        self.cross_size_range = config_data["cross_size_range"]
-        self.cross_thickness_range = config_data["cross_thickness_range"]
-        self.images_in_each_class = config_data["images_in_each_class"]
+        self.pattern_length = config_data["pattern_length"]
+        self.sequence_length = config_data["sequence_length"]
+        self.sequences = config_data["sequences"]
         self.noise_ratio = config_data["noise_ratio"]
-        self.flatten = config_data["flatten"]
-        self.center = config_data["center"]
 
     def __str__(self):
         x = "Arguments: {\n"
